58b13b0dcfadb42c6c52fc406e9ebfc4b225ee63
Marcos Ezequiel Rodriguez <marcos.rodriguez@globant.com>
2020-08-16 22:25:43 -0300

    ahora si fixeado bien

--- rick-morty/src/components/header/Header.js
+++ rick-morty/src/components/header/Header.js
@@ -7,7 +7,7 @@ import {rm} from '../../assets/rickMortyOk.svg';
 
 const Header = (props) => {
   let pathN = useLocation().pathname;
-  const [smallDevice, setSmallDevice] = useState(window.innerWidt>700)
+  const [smallDevice, setSmallDevice] = useState(window.innerWidth<700)
   let x = window.matchMedia("(min-width: 700px)");
     x.addListener(widthChange);
     function widthChange(x){
